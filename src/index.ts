320235c05a3797ae2395ef7c5d75813cb6895de3
Novvac <novvac21@protonmail.com>
2023-06-05 21:58:30 +0200

    chore: add eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Application works!');
-})
+  res.send('Application works!');
+});
 
 app.listen(3000, () => {
-    console.log('Application started on port 3000')
-})
+  console.log('Application started on port 3000');
+});
